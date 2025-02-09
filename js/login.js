0ceca5912a787713688fdacd44f1fec0a5739e1e
Tatsu821 <tatsuhiro821@outlook.jp>
2025-02-09 21:29:41 +0900

    login fix

--- js/login.js
+++ js/login.js
@@ -1,12 +1,13 @@
 // gasのエンドポイント
-const api_url = "https://script.google.com/macros/s/AKfycbx96C5DoWTdc7EF5LPZvuARAlVyoTI3Jj2UOLaCQvMlU-xoktEUz_wq4tyeCSSrqfDu/exec";
+// const api_url = "https://script.google.com/macros/s/AKfycbx96C5DoWTdc7EF5LPZvuARAlVyoTI3Jj2UOLaCQvMlU-xoktEUz_wq4tyeCSSrqfDu/exec";
+const api_url = "https://script.google.com/macros/s/AKfycbz4hlqWUi2IDAKfPjrqiJynoaSHYyQjznmwwOJcKaIlqLEdqTueXkKfDW6MAeJ2Td4vUA/exec"
 
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault(); // ページリロードを防止
     const username = document.getElementById("username").value;
-    const birthDate = new Date(document.getElementById("birthdate").value);
+    // const birthDate = new Date(document.getElementById("birthdate").value);
 
-    console.log(username, birthDate);
+    console.log(username);
 
     const loadingOverlay = document.getElementById("loadingOverlay");
     loadingOverlay.style.display = "block"; // ローディングアニメーションを表示
@@ -14,9 +15,9 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     fetch(api_url, {
         method: "post",
         headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
+            "Content-Type": "application/x-www-form-urlencoded"
         },
-        body: encodeURI(`name=${username}&birth_date=${birthDate.toLocaleString()}`)
+        body: encodeURI(`name=${username}`)
         })
         .then((response) => {
             response.json().then((json) => {
@@ -27,7 +28,25 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
                 }
                 // alert(json.message+"\nHello    "+json.name);
                 loadingOverlay.style.display = "none";
-                window.location.href = `/mypage.html#${json.name}`;
+
+                // 個人ID,名前,ナマエ,あだ名,支部,地区,パーティ,学年,性別,活動グループ,グループチャット,移動バス,まじりあえA,まじりあえB
+                const userInfo = {
+                    "id": decodeURIComponent(json.id),
+                    "name": decodeURIComponent(json.name),
+                    "nickname": decodeURIComponent(json.nickname),
+                    "branch": decodeURIComponent(json.branch),
+                    "district": decodeURIComponent(json.district),
+                    "party": decodeURIComponent(json.party),
+                    "grade": decodeURIComponent(json.grade),
+                    "activityGroup": decodeURIComponent(json.activityGroup),
+                    "groupChat": decodeURIComponent(json.groupChat),
+                    "bus": decodeURIComponent(json.bus),
+                    "mixA": decodeURIComponent(json.mixA),
+                    "mixB": decodeURIComponent(json.mixB),
+                }
+                console.log(userInfo);
+
+                window.location.href = `/mypage.html#${userInfo.id}`;
             });
 
         })
